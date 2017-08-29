15335a39f3c87a7127e41d6ccdc132af8bb6d74e
Sandeep JamithiReddy <c.sandeep.jamithireddy@uptake.com>
2017-08-29 13:59:56 -0500

    services and code clean up

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { StarWarsService } from '../star-wars.service';
 
 @Component({
   selector: 'app-item',
@@ -6,11 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-  character;
+  @Input() character;
+  swService: StarWarsService;
 
-  constructor() { }
+  constructor(swService: StarWarsService) {
+    this.swService = swService;
+   }
 
   ngOnInit() {
   }
 
+  onAssign(side) {
+    // this.character.side = side;
+    // this.sideAssigned.emit({name: this.character.name, side: side});
+
+    this.swService.onSideChosen({name: this.character.name, side: side});
+  }
 }
--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StarWarsService } from '../star-wars.service';
 
 @Component({
   selector: 'app-tabs',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
+characters = [];
+  chosenList = 'all';
+  swService: StarWarsService;
 
-  constructor() { }
+  constructor(swService: StarWarsService) {
+    this.swService = swService;
+   }
 
   ngOnInit() {
   }
 
+onChoose(side) {
+  this.chosenList = side;
+}
+
+getCharacters() {
+  this.characters = this.swService.getCharacters(this.chosenList);
+  return this.characters;
+}
 }
